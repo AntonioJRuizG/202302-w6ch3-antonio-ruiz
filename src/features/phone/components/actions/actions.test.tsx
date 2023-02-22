63bd19fb15ea1b178dd7110e2f6d28f7caa728e2
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-22 20:52:29 +0100

    Fix sonar smells

--- src/features/phone/components/actions/actions.test.tsx
+++ src/features/phone/components/actions/actions.test.tsx
@@ -1,5 +1,4 @@
-import { screen } from "@testing-library/react";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { Actions } from "./actions";
 describe("Given App component", () => {
   describe("When it is rendered", () => {
--- src/features/phone/components/display/display.test.tsx
+++ src/features/phone/components/display/display.test.tsx
@@ -1,5 +1,4 @@
-import { screen } from "@testing-library/react";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { Display } from "./display";
 
 describe("Given App component", () => {
