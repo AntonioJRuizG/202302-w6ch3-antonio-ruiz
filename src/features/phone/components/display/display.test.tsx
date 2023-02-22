337c915a9eb5667e082ffb1c7e18b469d369fcb3
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-23 00:30:09 +0100

    Test display and keyboard

--- src/features/phone/components/display/display.test.tsx
+++ src/features/phone/components/display/display.test.tsx
@@ -1,10 +1,16 @@
 import { screen, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../../../core/store/store";
 import { Display } from "./display";
 
 describe("Given App component", () => {
   describe("When it is rendered", () => {
     test("Then it should call Info, Keyboard, Display and Actions components", () => {
-      render(<Display></Display>);
+      render(
+        <Provider store={store}>
+          <Display></Display>
+        </Provider>
+      );
       const element = screen.getByRole("none");
       expect(element).toBeInTheDocument();
     });
--- src/features/phone/components/keyboard/keyboard.test.tsx
+++ src/features/phone/components/keyboard/keyboard.test.tsx
@@ -1,9 +1,15 @@
 import { screen, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../../../core/store/store";
 import { Keyboard } from "./keyboard";
 describe("Given App component", () => {
   describe("When it is rendered", () => {
     test("Then it should call Info, Keyboard, Display and Actions components", () => {
-      render(<Keyboard></Keyboard>);
+      render(
+        <Provider store={store}>
+          <Keyboard></Keyboard>
+        </Provider>
+      );
       const element = screen.getAllByRole("listitem");
       expect(element[0]).toBeInTheDocument();
     });
